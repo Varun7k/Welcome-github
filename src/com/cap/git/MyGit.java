181a5ef07ffabf53814b71f73b12b7f3fdf59dc2
Varun7k <kumarabc21@gmail.com>
2022-03-22 09:06:06 +0530

    changes from potti

--- src/com/cap/git/MyGit.java
+++ src/com/cap/git/MyGit.java
@@ -3,5 +3,7 @@ package com.cap.git;
 public class MyGit {
 public static void main(String[] args) {
 	System.out.println("Welcome to github");
+	//branch potti
+	System.out.println("this is branch potti");
 }
 }
